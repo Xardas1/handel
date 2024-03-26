d1d5f4595ca943268077f3d715286ae802a55d6d
ping010 <ping0.wenta@gmail.com>
2024-03-26 21:14:58 +0100

    dt

--- test.py
+++ test.py
@@ -4,13 +4,10 @@ while 1:
 def mama_blow():
     return 'cycki'
 
-<<<<<<< HEAD
 def jd():
     return "olek"
-=======
 print ("Łukasz lubi Kalinke")
 
 
 
 #Dupa Tymona
->>>>>>> 633d1366e69b6094e0fa5f289a1e52e45378a384
