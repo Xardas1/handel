5ac04d68aef69da64ca3e8046f8c956309cba760
Xardas1 <ciufel.maly@gmail.com>
2024-04-03 00:24:26 +0200

    api

--- test.py
+++ test.py
@@ -1,5 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import alpaca_trade_api as tradeapi
+
+
+API_KEY = "TU BZRYTOSZ DA API KEY"
+API_SECRET = "TU BZRYTOSZ DA API SECRET ZE SWOJEJ STRONY"
+API_URL = "https://api.alpaca.markets" 
+
+
+api = tradeapi.REST(API_KEY, API_SECRET, API_URL, api_version='v2')
+
+
+api.submit_order(
+    symbol='AAPL' #Firma apple
+    qty=1 # Ile chcemy kupić akcji
+    side='sell' #Czy chcemy sprzedać czy kupuić
+    type='market' #Sposób w jaki chcemy chcemy kupować akcję bzrytosz ogarnię swoją teorią
+    time_in_force='gtc' #Czas trawania ile order będze aktywny zanim wygaśnie
+)
+
+
+
+
 
 
 def calculate_difference():
@@ -10,4 +32,4 @@ def calculate_difference():
     distance_from_x_axis = np.abs(y)
     return distance_from_x_axis[below_x]
 
-print(calculate_difference())
+#print(calculate_difference())
